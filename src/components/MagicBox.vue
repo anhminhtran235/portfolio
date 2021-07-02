69c2aa5ea76955f32cfe6b4593b0bef3cd82eab1
Anh Minh Tran <anhminhtran235@gmail.com>
2021-07-01 20:45:35 -0700

    Add magic box

---
+++ src/components/MagicBox.vue
@@ -0,0 +1,110 @@
+<template>
+    <button id="btn" class="magic">Magic 🎩</button>
+    <div id="boxes" class="boxes big"></div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        const boxesContainer = document.getElementById('boxes')
+        const btn = document.getElementById('btn')
+
+        btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+        function createBoxes() {
+            for (let i = 0; i < 4; i++) {
+                for (let j = 0; j < 4; j++) {
+                  const box = document.createElement('div')
+                  box.classList.add('box')
+                  box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+                  boxesContainer.appendChild(box)
+                }
+            }
+        }
+
+        createBoxes()
+    }
+}
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+.magic {
+  background-color: #f9ca24;
+  color: #fff;
+  font-family: 'Poppins', sans-serif;
+  border: 0;
+  border-radius: 3px;
+  font-size: 16px;
+  padding: 12px 20px;
+  cursor: pointer;
+  position: fixed;
+  top: 20px;
+  letter-spacing: 1px;
+  box-shadow: 0 3px rgba(249, 202, 36, 0.5);
+  z-index: 100;
+}
+
+.magic:focus {
+  outline: none;
+}
+
+.magic:active {
+  box-shadow: none;
+  transform: translateY(2px);
+}
+
+.boxes {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  height: 500px;
+  width: 500px;
+  position: relative;
+  transition: 0.4s ease;
+}
+
+.boxes.big {
+  width: 600px;
+  height: 600px;
+}
+
+.boxes.big .box {
+  transform: rotateZ(360deg);
+}
+
+.box {
+  background-image: url('https://media.giphy.com/media/EZqwsBSPlvSda/giphy.gif');
+  background-repeat: no-repeat;
+  background-size: 500px 500px;
+  position: relative;
+  height: 125px;
+  width: 125px;
+  transition: 0.4s ease;
+}
+
+.box::after {
+  content: '';
+  background-color: #f6e58d;
+  position: absolute;
+  top: 8px;
+  right: -15px;
+  height: 100%;
+  width: 15px;
+  transform: skewY(45deg);
+}
+
+.box::before {
+  content: '';
+  background-color: #f9ca24;
+  position: absolute;
+  bottom: -15px;
+  left: 8px;
+  height: 15px;
+  width: 100%;
+  transform: skewX(45deg);
+}
+</style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,17 +1,20 @@
 <template>
     <container>
         <navbar></navbar>
+        <magic-box></magic-box>
     </container>
 </template>
 
 <script>
 import Container from '../components/Container.vue';
 import Navbar from '../components/Navbar.vue';
+import MagicBox from '../components/MagicBox.vue';
 
 export default {
     components: {
         Container,
         Navbar,
+        MagicBox
     },
 };
 </script>
